5287ec8c9709b1a92c69ec051ad7577df1df92af
ShauryaSuhane <suhaneshaurya26@gmail.com>
2025-05-23 09:44:04 +0530

    greater or smaller

---
+++ greater or smaller.py
@@ -0,0 +1,8 @@
+i=int(input("Enter the number:"))
+if i>5:
+    print("The number is greater")
+    print("This is in the if block")
+else:
+    print("This number is smaller")
+    print("This is in the else block")
+print("They both are not in the if and else block")
